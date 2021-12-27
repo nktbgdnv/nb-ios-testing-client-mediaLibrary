d29945df7439fc50b2700e39449fcce5de3354b4
Nikita Bogdanov <94480702+nktbgdnv@users.noreply.github.com>
2021-12-27 21:57:57 +0300

    Add files via upload

--- nb-ios-testing-client-mediaLibrary.playground/Contents.swift
+++ nb-ios-testing-client-mediaLibrary.playground/Contents.swift
@@ -1,5 +1,11 @@
+/// Данный код не претендует на полноценную музыкальную библиотеку, однако
+/// он с достаточной точностью реализует ее основные функции на Swift.
+/// А это - хранение в коллекции данных треков, выгрузка "плейлистов" по определенному
+/// жанру, добавление и удаление треков из "медиатеки".
+/// N. Bogdanov (c)
 
-// структура Трек
+
+// структура Track, содержащая свойства-информацию о музыкальном треке
 struct Track {
     var name: String
     var artist: String
@@ -10,7 +16,7 @@ struct Track {
 
 // класс "Медиатека" (музыкальная библиотека)
 public class MediaLibrary {
-    // массив с названиями плейлистов
+    // плейлисты-массивы
     var listOfPlaylists: [String] = []
     
     // метод по агрегации всех плейлистов
@@ -19,13 +25,13 @@ public class MediaLibrary {
     }
 }
 
-// класс - абстракция "Плейлист (категория треков)"
+// класс-абстракция "Плейлист (категория треков)"
 public class FirstPlaylist {
-    // хранимое свойство - название категории
+    // название категории
     var name: String
-    // хранимое свойство - список песен
+    // список песен
     var songList: [Track]
-    // вычислимое свойство - количество треков
+    // количество треков
     var numberOfTracks: Int {
         return songList.count
     }
@@ -39,7 +45,6 @@ public class FirstPlaylist {
         }
     }
     
-    // метод, удаляющий трек
     func delete(title: String) {
         var index = 0
         for song in self.songList {
@@ -81,7 +86,7 @@ let rapSongs = medialibrary.filter {$0.genre == "Rap"}
 // создаем экземпляр класса "Первый плейлист" - музыка в стиле Рэп
 var rapMusic: FirstPlaylist = FirstPlaylist(name: "Музыка жанра Rap/Hip-hop", songList: rapSongs)
 
-// добавляем новый элемент в категорию
+// добавляем новый элемент в категорию, проверяем, тестируем
 rapMusic.add(song: Track(name: "Monster", artist: "Eminem", duration: 4.10, country: "USA", genre: "Rap"))
 rapMusic.add(song: Track(name: "Monster", artist: "Eminem", duration: 4.10, country: "USA", genre: "Rap"))
 
@@ -91,7 +96,7 @@ rapMusic.delete(title: "Godzilla")
 
 print("Количество песен в плейлисте \"\(rapMusic.name)\": \(rapMusic.numberOfTracks)")
 
-// класс "Второй плейлист"
+// второй плейлист
 public class SecondPlaylist: FirstPlaylist {}
 
 // получаем из общей библиотеки только песни жанра "Рок"
@@ -99,9 +104,15 @@ let rockSongs = medialibrary.filter {$0.genre == "Rock"}
 
 // новый экземпляр класса "Второй плейлист" - музыка в стиле Рок
 var rockMusic: SecondPlaylist = SecondPlaylist(name: "Музыка жанра Рок", songList: rockSongs)
+
+// для проверки добавим новые песни в жанре "Рок"
+rockMusic.add(song: Track(name: "Unforgiven", artist: "Metallica", duration: 4.00, country: "USA", genre: "Rock"))
+rockMusic.add(song: Track(name: "Unforgiven II", artist: "Metallica", duration: 4.30, country: "USA", genre: "Rock"))
+rockMusic.add(song: Track(name: "Unforgiven III", artist: "Metallica", duration: 5.10, country: "USA", genre: "Rock"))
+
 print("Количество песен в плейлисте \"\(rockMusic.name)\": \(rockMusic.numberOfTracks)")
 
-// объект класса "Медиатека"
+// создаем объект класса "Медиатека"
 let homeMediaLibrary = MediaLibrary()
 homeMediaLibrary.addPlaylist(name: rapMusic.name)
 homeMediaLibrary.addPlaylist(name: rockMusic.name)
@@ -109,3 +120,122 @@ homeMediaLibrary.addPlaylist(name: rockMusic.name)
 // выводим на экран список наших плейлистов
 print("Мои домашние плейлисты: \(homeMediaLibrary.listOfPlaylists)")
 
+/// немного расширим наши абстракции - создаем новый класс "Артист" - он будет суперклассом
+class Artist {
+    var name: String = ""
+    var country: String = ""
+    var genre: String = ""
+    
+    // общий метод "написать трек"
+    public func writeTrack(artist: Artist, track: Track) {
+        print("Я \(artist.name) написал трек \(track.name)")
+    }
+    
+    // общий метод "исполнить трек"
+    public func performTrack(artist: Artist, track: Track) {
+        print("Я \(artist.name) исполнил трек \(track.name)")
+    }
+    
+    init(artistName: String, artistCountry: String, artistGenre: String) {
+        self.name = artistName
+        self.country = artistCountry
+        self.genre = artistGenre
+    }
+}
+
+// создание сабкласса "Одиночный исполнитель"
+final class Singer: Artist {
+    
+    // переопределение свойства суперкласса "страна"
+    override var country: String {
+        willSet {
+            print("Исполнитель \(self.name) переехал из страны: \(self.country) в страну: \(newValue)")
+        }
+    }
+    // переопределение свойства суперкласса "жанр"
+    override var genre: String {
+        willSet {
+            print("Исполнитель \(self.name) переехал из страны: \(self.country) в страну: \(newValue)")
+        }
+    }
+    // свойства "Количество альбомов" и "Количество одиночных композиций"
+    var numberOfAlbums: Int = 0
+    var numberOfSingles: Int = 0
+    // свойство "Выступает ли исполнитель с концертами"
+    var performConcerts: Bool
+    
+    // переопределение инициализатора
+    init(artistName: String,
+         artistCountry: String,
+         artistGenre: String,
+         performConcerts: Bool) {
+        self.performConcerts = performConcerts
+        super.init(artistName: artistName, artistCountry: artistCountry, artistGenre: artistGenre)
+    }
+}
+
+// создание сабкласса "Музыкальная группа"
+final class Group: Artist {
+    // переопределение свойства "Имя"
+    override var name: String {
+        willSet {
+            print("Группа \(self.name) сменила название! Теперь она называется \(newValue)")
+        }
+    }
+    
+    // переопределение свойства суперкласса "страна"
+    override var country: String {
+        willSet {
+            print("Группа \(self.name) переехала из страны: \(self.country) в страну: \(newValue)")
+        }
+    }
+    
+    // переопределение свойства суперкласса "жанр"
+    override var genre: String {
+        willSet {
+            print("У группы \(self.name) новый жанр \(newValue)")
+        }
+    }
+    // создание уникального свойства "Количество человек в группе"
+    var numberOfPeople: Int
+    // создание уникального свойства "Выступает ли группа с концертами"
+    var performConcerts: Bool
+    
+    // уникальный для прочих классов метод "Поиск по названию группы"
+    func searchAndPrintGroupSongs() {
+        let nameOftheGroup = self.name
+        let selectedSongs = rockMusic.songList.filter {$0.artist == nameOftheGroup}
+        print("Найдено \(selectedSongs.count) песен группы \(self.name) в плейлисте")
+    }
+    
+    // переопределение инициализатора
+    init(artistName: String,
+                  artistCountry: String,
+                  artistGenre: String,
+                  numberOfPeople: Int,
+                  performConcerts: Bool) {
+        self.numberOfPeople = numberOfPeople
+        self.performConcerts = performConcerts
+        super.init(artistName: artistName, artistCountry: artistCountry, artistGenre: artistGenre)
+    }
+}
+
+// создание экземпляра класса Group, проверка его значений
+var metallica: Group = Group(artistName: "Metallica", artistCountry: "USA", artistGenre: "Rock", numberOfPeople: 4, performConcerts: true)
+
+// другие группы и отдельные исполнители
+var offspring: Group = Group(artistName: "The Offspring", artistCountry: "USA", artistGenre: "Rock", numberOfPeople: 4, performConcerts: true)
+var sting: Singer = Singer(artistName: "Sting", artistCountry: "USA", artistGenre: "Pop", performConcerts: false)
+
+// проверка метода "Поиск песен выбранной группы в плейлисте"
+metallica.searchAndPrintGroupSongs()
+
+// проверим, сколько артистов присутствует в медиатеке
+var arstistsArray = [Artist]()
+arstistsArray.append(metallica)
+arstistsArray.append(offspring)
+arstistsArray.append(sting)
+print("В медиатеке найдено \(arstistsArray.count) артистов. Вот они:")
+for item in arstistsArray {
+    print(item.name)
+}
